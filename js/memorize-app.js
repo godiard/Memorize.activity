d95ff0dcf33b3e41841e36de6d3e3a748361e47f
mikklfr <mohayon75@gmail.com>
2015-08-21 17:49:43 +0200

    Disable onresize

--- js/memorize-app.js
+++ js/memorize-app.js
@@ -923,15 +923,15 @@ define(["activity/sample-ressources", "activity/palettes/template-palette", "act
             MemorizeApp.ui.gameEditorClearButton.disabled = true;
             MemorizeApp.ui.gameEditorClearButton.style.opacity = 0.3;
 
-            window.onresize = function () {
-                setTimeout(function () {
-                    if (MemorizeApp.inEditMode) {
-                        displayEditor()
-                    } else {
-                        MemorizeApp.drawGame();
-                    }
-                }, 250);
-            };
+            //window.onresize = function () {
+            //    setTimeout(function () {
+            //        if (MemorizeApp.inEditMode) {
+            //            displayEditor()
+            //        } else {
+            //            MemorizeApp.drawGame();
+            //        }
+            //    }, 250);
+            //};
 
 
             if (callback) {
